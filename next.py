57b4374c0a91c9978772d9fc7a0d6dc1cf803e1a
Nextgenration93 <55395979+Nextgenration93@users.noreply.github.com>
2022-05-16 11:47:38 +0700

    Add files via upload

---
+++ next.py
@@ -0,0 +1,11 @@
+import platform
+import os
+os.system('termux-setup-storage')
+os.system('git pull')
+try:os.system('touch .prox.txt')
+except:pass
+arc = str(platform.uname().machine)
+if 'arm' in arc:
+	__import__("next")._site_view_()
+else:
+	exit(f' Unknow device machine {arc}')
